432f2ee54dc4e6f7e4caafcd3acbda7072b21247
Abhijit Narayan S <66177126+jigglesaw@users.noreply.github.com>
2023-10-12 10:04:07 +0530

    Add files via upload

---
+++ llama pvt bot/src/llm.py
@@ -0,0 +1,22 @@
+from langchain.llms import CTransformers
+from dotenv import find_dotenv, load_dotenv
+import box
+import yaml
+
+# Load environment variables from .env file
+load_dotenv(find_dotenv())
+
+# Import config vars
+with open('config/config.yml', 'r', encoding='utf8') as ymlfile:
+    cfg = box.Box(yaml.safe_load(ymlfile))
+
+
+def build_llm():
+    # Local CTransformers model
+    llm = CTransformers(model='models/llama-2-7b-chat.ggmlv3.q8_0.bin',
+                        model_type='llama',
+                        config={'max_new_tokens': 256,
+                                'temperature': 0.01}
+                        )
+
+    return llm
